975ce9a98c276ac35a2dfa48aa7974a80d47603c
Malle Alaru <malle.alaru@khk.ee>
2019-11-25 20:30:36 +0200

    Ylesanne 5 metshein ained pallivisked nimed count array_sum

--- arrays/ylesanne5.php
+++ arrays/ylesanne5.php
@@ -1,5 +1,15 @@
 <?php
-$nimed = array('mari', 'kati', 'juhan', 'miku', 'uku');
+$ained = array('matemaatika', 'muusika', 'inglise keel', 'kehaline kasvatus', 'eesti keel');
 //sorteerimine
-sort($nimed);
-var_dump($nimed);
+sort($ained);
+
+foreach($ained as $aine){
+    echo "$aine <br>";
+}
+
+$nimed = array('Martin','Hardi','Juhan','Tiina','Sirje','Kaie');
+$pallivisked = array(33, 32, 27, 11, 15, 28);
+
+var_dump(count($nimed));
+var_dump(array_sum($pallivisked));
+
