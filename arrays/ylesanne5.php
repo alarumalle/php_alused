6dbef277ec7330f07868bb3ce1dae19cd6190600
Malle Alaru <malle.alaru@khk.ee>
2019-11-25 21:11:26 +0200

    Ylesanne 5 metshein array_combine ja array_keys

--- arrays/ylesanne5.php
+++ arrays/ylesanne5.php
@@ -1,15 +1,20 @@
 <?php
 $ained = array('matemaatika', 'muusika', 'inglise keel', 'kehaline kasvatus', 'eesti keel');
 //sorteerimine
-sort($ained);
+//sort($ained);
+//var_dump($ained);
 
 foreach($ained as $aine){
     echo "$aine <br>";
 }
 
+
 $nimed = array('Martin','Hardi','Juhan','Tiina','Sirje','Kaie');
 $pallivisked = array(33, 32, 27, 11, 15, 28);
-
+//
 var_dump(count($nimed));
 var_dump(array_sum($pallivisked));
-
+var_dump(max($pallivisked));
+$a = array_combine($nimed, $pallivisked);
+print_r(array_keys($a)); //ainult v6tmed?
+print_r(array_search(max($a), $a));
